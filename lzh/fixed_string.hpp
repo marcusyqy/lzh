278309d8a37918393610b6241f5074737733d786
Marcus Yong <marcusyqy@gmail.com>
2022-08-09 14:49:20 +0800

    changed to size_t and removed `unsigned`. removed `string_view` due to bugs that will not be able to solve

--- lzh/fixed_string.hpp
+++ lzh/fixed_string.hpp
@@ -1,22 +1,17 @@
+#include <cstdint>
 #include <string_view>
 
 namespace lzh {
-
 /*
  * this class is mainly for being able to write
  * hello<"hello"> in template arguments
  * */
-template<unsigned N>
+template<size_t N>
 struct fixed_string {
     char buf[N + 1]{};
 
     constexpr fixed_string(char const* s) {
-        for (unsigned i = 0; i != N; ++i)
-            buf[i] = s[i];
-    }
-
-    constexpr fixed_string(std::string_view s) {
-        for (unsigned i = 0; i != N; ++i)
+        for (size_t i {}; i != N; ++i)
             buf[i] = s[i];
     }
 
@@ -25,7 +20,7 @@ struct fixed_string {
     auto operator<=>(const fixed_string&) const = default;
 };
 
-template<unsigned N>
+template<size_t N>
 fixed_string(char const (&)[N]) -> fixed_string<N - 1>;
 
 } // namespace lzh
